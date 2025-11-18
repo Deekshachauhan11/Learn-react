2fa870074bfb9bd304d04543ec13fa8488b513f0
Deekshachauhan11 <cdeeksha0011@gmail.com>
2025-11-18 16:48:07 +0530

    gallery project done

--- 15-use-Effect/src/App.jsx
+++ 15-use-Effect/src/App.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  function Achanging(){
+    console.log("A ki value cange ho gyi")
+  }
+  
+  function Bchanging(){
+    console.log("B ki value cange ho gyi")
+  }
+
+  useEffect(function () {
+    Achanging()
+    Bchanging()
+  }, [a, b]);
+
   return (
     <div>
-      App
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+      <button
+        onClick={() => {
+          setA(a + 1);
+        }}
+      >
+        Click A
+      </button>
+      <button
+        onClick={() => {
+          setB(b - 1);
+        }}
+      >
+        Click B
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
